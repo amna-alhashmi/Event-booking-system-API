09a3f45478a54ab22d68bb0ea5af6516c8f74342
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-22 15:50:04 +0400

    add preAuthorize for create ticket

--- src/main/java/com/example/UserRegistration/Controller/TicketBookingController.java
+++ src/main/java/com/example/UserRegistration/Controller/TicketBookingController.java
@@ -6,6 +6,7 @@ import com.example.UserRegistration.Models.UserRegistration;
 import com.example.UserRegistration.RequestObject.TicketRequest;
 import com.example.UserRegistration.Service.TicketBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -16,22 +17,25 @@ import java.util.List;
 public class TicketBookingController {
     @Autowired
     TicketBookingService ticketBookingService;
-
+    @PreAuthorize("hasRole('USER')")
     @RequestMapping(value = "addTicketBooking", method = RequestMethod.POST)
     public String addNewTicket(@RequestBody TicketRequest ticketRequest) {
         ticketBookingService.addNewTicket(ticketRequest);
 
         return "Ticket add successful";
     }
+
     @RequestMapping(value = "getAllBooking", method = RequestMethod.GET)
     public Integer getBookingByNumberOfTicket(Integer eventId) throws ParseException {
         return ticketBookingService.getBookingByNumberOfTicket(eventId);
 
     }
+
     @RequestMapping(value = "getAllValues",method = RequestMethod.GET)
     public List<TicketBooking> getAllTicket1(){
         return ticketBookingService.getAllTicket1();
     }
+
     @RequestMapping(value = "BookingCancellation",method = RequestMethod.POST)
     public void BookingCancellation(@RequestParam Integer bookingId){
         ticketBookingService.BookingCancellation(bookingId);
