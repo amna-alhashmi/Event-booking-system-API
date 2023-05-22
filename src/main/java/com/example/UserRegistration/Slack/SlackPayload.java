a0ac14ba1bdd00e9aa04ed9200661166021eaa91
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-22 08:52:57 +0400

    update ticket availability for every 5 minutes

--- src/main/java/com/example/UserRegistration/Slack/SlackPayload.java
+++ src/main/java/com/example/UserRegistration/Slack/SlackPayload.java
@@ -1,4 +1,15 @@
 package com.example.UserRegistration.Slack;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
 public class SlackPayload {
+    private String text;
+    public SlackPayload (String text){
+        this.text=text;
+    }
 }
