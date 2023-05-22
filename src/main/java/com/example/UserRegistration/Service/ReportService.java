c90830809baa0ace8cce05314b1f10cd4a3249a7
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-22 10:42:53 +0400

    do autowired for each interface

--- src/main/java/com/example/UserRegistration/Service/ReportService.java
+++ src/main/java/com/example/UserRegistration/Service/ReportService.java
@@ -1,4 +1,18 @@
 package com.example.UserRegistration.Service;
 
+import com.example.UserRegistration.Repositories.EventInterface;
+import com.example.UserRegistration.Repositories.TicketBookingInterface;
+import com.example.UserRegistration.Repositories.UserRegistrationInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReportService {
+@Autowired
+    UserRegistrationInterface userRegistrationInterface;
+@Autowired
+    EventInterface eventInterface;
+@Autowired
+    TicketBookingInterface ticketBookingInterface;
+
 }
